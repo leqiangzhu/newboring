cbea4ca96b06311843ee362ead2402c07180db3a
David Zhu and Kenneth Du <kenneth@>
2018-08-07 10:38:20 -0700

    Add Jquery to toggle P text with a border and background color

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("p").click(function() {
+    $("p").removeClass();
+    $("p").addClass("border");
+  });
+
+  $("p").dblclick(function() {
+    $("p").removeClass();
+    $("p").addClass("highlight");
+  });
+
+
+
+
+    });
